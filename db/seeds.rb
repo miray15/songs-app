2260dc7c9b91a4f83fea6231247eac754025f3a1
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-31 12:21:20 -0500

    add 4 songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Song.create(title: "Take On Me", album: "idk", artist: "a-ha", year: 1985)
+
+Song.create(title: "Never Gonna Give You Up", album: "idkk", artist: "Rick Astley", year: 1987)
+
+Song.create(title: "Tainted Love", album: "hmm", artist: "Soft Cell", year: 1981)
+
+Song.create(title: "Call Me", album: "wow no clue", artist: "Blondie", year: 1980)
+
